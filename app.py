29498eb14f570b6189326c1a191be9e9a113d540
hrdkmishra <hardikmishra0509@gmial.com>
2023-03-22 00:29:02 +0530

    added `login.html` and flashed message at `default.html` using flash by sending category to bootstrap.

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '777a8e3e7f7b33e356669e1a2668a3d6'
 
 posts = [
     {
@@ -27,6 +30,21 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='Abouts')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login')
 
 
 if __name__ == '__main__':
