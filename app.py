f27c551a8ccfb5155202bda7b1c9f885f7342ebd
hrdkmishra <hardikmishra0509@gmial.com>
2023-03-21 19:43:42 +0530

    bootstrap added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -11,7 +11,7 @@ posts = [
     },
     {
         'author': 'Modi Mishra',
-        'title': 'Blog Post 1',
+        'title': 'Blog Post 2',
         'content': 'Second post content',
         'date_posted': 'April 21, 2018'
     }
@@ -26,7 +26,7 @@ def home():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='Abouts')
 
 
 if __name__ == '__main__':
